7b9389df826357447ef513105010799ef12b7e5a
VanyaKootchin <vanya.kootchin@maestronic.ca>
2023-03-13 12:43:05 -0400

    sh

---
+++ split_src/run.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+if mpicc -c cpu.c -o cpu.o; then 
+    if nvcc -c gpu.cu -o gpu.o; then
+        mpicc cpu.o gpu.o -lcudart
+    else
+        echo "\033[1;31mGPU compilation failed\e[0m"
+    fi
+else
+    echo "\033[1;31mCPU compilation failed\e[0m"
+fi
