8779a128040eba40fa873e334fe0323483f5920d
VanyaKootchin <vanya.kootchin@maestronic.ca>
2023-03-13 20:18:48 -0400

    shell

--- split_src/run.sh
+++ split_src/run.sh
@@ -1,13 +1,24 @@
 #!/bin/bash
-if mpicc -c cpu.c -o cpu.o; then 
-    if nvcc -c gpu.cu -o gpu.o; then
-        if mpicc cpu.o gpu.o -lcudart; then
-            ./a.out
+
+if module load openmpi cuda; then 
+    if mpicc -c cpu.c -o cpu.o; then 
+        if nvcc -c gpu.cu -o gpu.o; then
+            if mpicc cpu.o gpu.o -lcudart; then
+                ./a.out
+            fi
+        else
+            echo "GPU compilation failed"
         fi
     else
-        echo "GPU compilation failed"
+        echo "CPU compilation failed"
     fi
-else
-    echo "CPU compilation failed"
 fi
 
+
+
+if 0; then
+    mpicc -c cpu.c -o cpu.o
+    nvcc -c gpu.cu -o gpu.o
+    mpicc cpu.o gpu.o -lcudart
+    ssh vmck18@graham.computecanada.ca
+fi
