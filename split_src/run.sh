debf2868533d6c878d53c0f712f37b3c4548a002
VanyaKootchin <vanya.kootchin@maestronic.ca>
2023-03-13 20:37:00 -0400

    run

--- split_src/run.sh
+++ split_src/run.sh
@@ -2,7 +2,7 @@
 
 if module load openmpi cuda; then 
     if mpicc -c cpu.c -o cpu.o; then 
-        if nvcc -c gpu.cu -o gpu.o; thennano 
+        if nvcc -c gpu.cu -o gpu.o; then
             if mpicc cpu.o gpu.o -lcudart; then
                 ./a.out
             fi
